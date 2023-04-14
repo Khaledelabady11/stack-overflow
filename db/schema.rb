05c8fdb7f96f35d2037a066b3bb11aa22819e6f5
Khaledelabady11 <k7aled.el3bady123@gmail.com>
2023-04-14 16:57:11 +0200

    Created Question and Answer Models and added validations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_04_13_054417) do
+ActiveRecord::Schema[7.0].define(version: 2023_04_14_145155) do
+  create_table "answers", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_answers_on_user_id"
+  end
+
+  create_table "questions", force: :cascade do |t|
+    t.string "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_questions_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -20,4 +36,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_13_054417) do
     t.string "token"
   end
 
+  add_foreign_key "answers", "users"
+  add_foreign_key "questions", "users"
 end
